f26eb4e2d09d63a25afb229fa976a9f1382b459e
shaktisat03 <124371948+shaktisat03@users.noreply.github.com>
2023-06-10 10:44:01 +0530

    Create rotate_matrix.cpp

---
+++ DAY 2/rotate_matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        for(int i=0;i<row; i++){
+            for(int j=i; j<row;j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for(int i=0;i<row;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
