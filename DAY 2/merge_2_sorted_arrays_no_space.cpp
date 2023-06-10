e37617030c4da30dec23d44273f803b0c96984f9
shaktisat03 <124371948+shaktisat03@users.noreply.github.com>
2023-06-10 10:45:54 +0530

    Create merge_2_sorted_arrays_no_space.cpp

---
+++ DAY 2/merge_2_sorted_arrays_no_space.cpp
@@ -0,0 +1,21 @@
+vector<int> ninjaAndSortedArrays(vector<int>& arr1, vector<int>& arr2, int m, int n) {
+	// Write your code here.
+	int i=m-1; 
+	
+	
+	int k= m+n-1;
+	int j = n-1;
+    while(j>=0){
+		if( i>=0 && (arr1[i]>arr2[j])){
+			arr1[k] = arr1[i];
+                  k--;
+                  i--;
+                } else {
+                  
+				  arr1[k]=arr2[j];
+			k--;
+			j--;
+                }
+    }
+    return arr1;
+}
